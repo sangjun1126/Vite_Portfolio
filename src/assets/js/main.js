740c2b27de7d88b5de46d731dd70d2e5ef8d8647
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-12 18:03:55 +0900

    레이아웃 틀 완성

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,21 +1,11 @@
-import '../css/style.css'
+import {menu} from './menu'
+import {port} from './port'
+import {link} from './link'
+import {smooth} from './smooth'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+window.addEventListener("load", function() {
+  smooth();
+  link();
+  menu();
+  port();
+})
