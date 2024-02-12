e34124015a0a2c2f348863e7d5be4d2f366ca74f
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-12 18:38:50 +0900

    인트로 부분 성공

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,13 @@
 export function menu() {
-    console.log(menu);
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector('.header__nav');
+
+    if (headerToggle) {
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+            headerToggle.getAttribute("aria-expanded") === "true"
+            ? headerToggle.setAttribute("aria-expanded", "false")
+            : headerToggle.setAttribute("aria-expanded", "true");
+        });
+    }
 }
