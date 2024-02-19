c6147f3e10e31de8eb0a1a1d7ddb4a9cd4e7debf
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-20 02:35:19 +0900

    드디어 마무리!

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,3 +1,14 @@
 export function link() {
-    console.log(link);
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({behavior : "smooth"})
+            }
+        })
+    })
 }
--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,3 +1,19 @@
+import Lenis from "@studio-freight/lenis";
+
 export function smooth() {
-    console.log(smooth);
+    const lenis = new Lenis({
+        duration : 1,
+        easing : (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf);
+
+    lenis.on('scroll', (e) => {
+        console.log(e);
+    })
 }
