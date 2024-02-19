e89c494de37871cc26361c82f74f5350554dc62f
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-20 01:24:05 +0900

    포트폴리오 완성

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,22 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 export function port() {
-    console.log(port);
+    gsap.registerPlugin(ScrollTrigger);
+    const horSection = gsap.utils.toArray(".port__item");
+
+    gsap.to(horSection, {
+        xPercent : -120 * (horSection.length - 1),
+        ease : "none",
+        scrollTrigger : {
+            trigger : "#port",
+            start : "top 56px",
+            end : "+=3000",
+            pig : true,
+            scrub : 1,
+            markers : false,
+            invalidateOnRefresh : true,
+            anticipatePin : 1,
+        }
+    })
 }
